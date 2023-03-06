43cfee47662ae49757edbdf5a078677fee058bed
danys <danys@PC-di-Daniela.Home>
2023-03-06 18:32:19 +0100

    svolgimento fino al punto 5 compreso

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,73 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	/**
+	 * Aggiungi un nuovo voto al libretto
+	 * (per ora non fa nessun controllo)
+	 * @param v il Voto da aggiungere
+	 * @return true
+	 */
+	public boolean add(Voto v) {
+		return this.voti.add(v);
+	}
+	
+	public void stampa() {
+		for(Voto v : this.voti) {
+			System.out.println(v);
+		}
+	}
+	
+	public List<Voto> getVoti(){
+		return this.voti;
+	}
+	
+	public void stampaPuntiUguali(int valore) {
+		for(Voto v : this.voti) {
+			if(v.getPunti() == valore)
+				System.out.println(v);
+		}
+	}
+		
+	public Voto cercaVotoPerNome(String corso) {
+		for(Voto v : this.voti) {
+			if(v.getCorso().equals(corso)) {
+				return v;
+			}
+		}
+		return null;
+		
+		// throw new RuntimeException("Voto non trovato"); usiamo l'eccezione x eventi imprevisti
+	}
+	
+	public boolean esisteVoto(Voto nuovo) {
+		for(Voto v: this.voti) {
+			if(v.getCorso().equals(nuovo.getCorso()) && v.getPunti() == nuovo.getPunti()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean esisteConflitto(Voto nuovo) {
+		for(Voto v: this.voti) {
+			if(v.getCorso().equals(nuovo.getCorso())) {
+				if(v.getPunti() != nuovo.getPunti()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,41 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Libretto lib = new Libretto();
+		
+		//1
+		lib.add(new Voto ("Analisi I", 29, LocalDate.of(2021, 2, 15)));
+		lib.add(new Voto ("Informatica", 25, LocalDate.of(2021, 2, 20)));
+		lib.add(new Voto ("Fisica I", 21, LocalDate.of(2022, 6, 10)));
+		
+		//2
+		lib.stampaPuntiUguali(25);
+		
+		//3
+		Voto votoCercato = lib.cercaVotoPerNome("Analisi I");
+		System.out.println(votoCercato);
+		
+		//4
+		Voto a1bis = new Voto ("Analisi I", 29, LocalDate.of(2025, 2, 15));
+		Voto a1ter = new Voto ("Analisi I", 30, LocalDate.of(2025, 2, 15));
+		
+		System.out.println(a1bis+" è duplicato "+lib.esisteVoto(a1bis));
+		System.out.println(a1ter+" è duplicato "+lib.esisteVoto(a1ter));
+		
+		//5
+		Voto f1bis = new Voto ("Fisica I", 20, LocalDate.of(2025, 6, 10));
+		Voto f1ter = new Voto ("Fisica I", 21, LocalDate.of(2025, 6, 10));
+		
+		System.out.println(f1bis+" è in conflitto "+lib.esisteConflitto(f1bis));
+		System.out.println(f1ter+" è in conflitto "+lib.esisteConflitto(f1ter));
+		
+		
 	}
 
 }
